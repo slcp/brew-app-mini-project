e29bd65b4f62158965996f85ab14e2ab6c0953dc
Stuart Forrest <stuart.forrest@infinityworks.com>
2020-09-16 08:35:10 +0100

    add people/drinks, add/view favourites

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import os
 # Menu options
 GET_PEOPLE_ARG = 1
 GET_DRINKS_ARG = 2
+ADD_PERSON_ARG = 3
+ADD_DRINK_ARG = 4
+SET_FAVOURITE_ARG = 5
+VIEW_FAVOURITES_ARG = 6
+EXIT_ARG = 7
 
 # CLI menu
 APP_NAME = 'BrIW'
@@ -14,18 +19,27 @@ Please, select an option by entering a number:
 
 [1] Get all people
 [2] Get all drinks
-[3] Exit
+[3] Add a person
+[4] Add a drink
+[5] Set a favourite drink
+[6] View favourites
+[7] Exit
 '''
+
+# App data
+drinks = ['Tea', 'Coffee', 'Mocha', 'Cortado']
+people = ['Malik', 'Katie', 'Taishan', 'Suman']
+favourite_drinks = {}
 
 
 # Table output helper funcs
-def get_table_width(items):
-    longest = 0
-    for item in items:
+def get_table_width(title, data):
+    longest = len(title)
+    additional_spacing = 2
+    for item in data:
         if len(item) > longest:
             longest = len(item)
-    # Add some spacing to the end of the table
-    return longest + 2
+    return longest + additional_spacing
 
 
 def print_table_body(contents):
@@ -48,7 +62,7 @@ def print_table_footer(width):
 
 
 def print_table(title, contents):
-    width = get_table_width(contents)
+    width = get_table_width(title, contents)
     print_table_header(title, width)
     print_table_body(contents)
     print_table_footer(width)
@@ -58,40 +72,119 @@ def clear_screen():
     os.system('clear')
 
 
-def show_menu():
+def print_main_menu():
     clear_screen()
     print(MENU_TEXT)
 
 
-def get_selection():
-    return int(input('Enter your selection: '))
+# Input helper funcs
+def get_menu_input(message):
+    try:
+        return int(input(f'{message} '))
+    except ValueError:
+        output("Menu items - numbers are the only input I understand")
+        wait()
+        return False
+
+
+def get_raw_input(message):
+    return input(f'{message} ')
+
+
+def validate_menu_input(index, data):
+    if index < 0 or index >= len(data):
+        print(f'"{index}" is not a valid option from that menu\n')
+        wait()
+        return False
+    return True
+
+
+def select_from_menu(message, data):
+    print_menu(message, data)
+    index = get_menu_input('Enter your selection:')
+    if not validate_menu_input(index, data):
+        return False
+    return data[index]
 
 
 def wait():
     input('\nPress any key to return to the main menu')
 
 
+# Output helper funcs
 def output(text):
     print(f'\n{text}')
 
 
-drinks = ['Tea', 'Coffee', 'Mocha', 'Cortado']
-people = ['Malik', 'Katie', 'Taishan', 'Suman']
+def print_people():
+    print_table('people', people)
 
+
+def print_drinks():
+    print_table('drinks', drinks)
+
+
+def print_menu(title, data):
+    items = []
+    for i, item in enumerate(data, start=1):
+        items.append(f'[{i}] {item}')
+    clear_screen()
+    print(f'{title}\n')
+    print('\n'.join(items), '\n')
+
+
+def print_favourites(data):
+    items = []
+    for name, drink in data.items():
+        items.append(f'{name}: {drink}')
+    print_table('Favourites', items)
+
+
+# Entry point
 while True:
-    show_menu()
-    try:
-        option = get_selection()
-        # Handle command
-        if option == GET_DRINKS_ARG:
-            print_table('drinks', drinks)
+    print_main_menu()
+    option = get_menu_input('Enter your selection:')
+    # There is no 0 menu option so this works, not ideal
+    if not option:
+        wait()
+        continue
+
+    # Handle command
+    if option == GET_DRINKS_ARG:
+        print_drinks()
+        wait()
+    elif option == GET_PEOPLE_ARG:
+        print_people()
+        wait()
+    elif option == EXIT_ARG:
+        print(f'Thank you for using {APP_NAME}')
+        exit()
+    elif option == ADD_PERSON_ARG:
+        name = get_raw_input("What is the name of the user?")
+        if name not in people:
+            people.append(name)
+        wait()
+    elif option == ADD_DRINK_ARG:
+        drink = get_raw_input("What is the name of the drink?")
+        if drink not in drinks:
+            drinks.append(drink)
+        wait()
+    elif option == SET_FAVOURITE_ARG:
+        person = select_from_menu('Choose a person', people)
+        if not person:
+            output(f'{person} is mo')
             wait()
-        elif option == GET_PEOPLE_ARG:
-            print_table('people', people)
+
+        drink = select_from_menu(f'Choose a drink for {person}', drinks)
+        if not drink:
             wait()
-        else:
-            output(f'"{option}"" is not an option that I recognise')
-            wait()
-    except ValueError:
-        output("Menu items numbers are the only input I understand")
+
+        favourite_drinks[person] = drink
+        print(f"\nThank you - {person}'s favourite drink is now {drink}")
+        wait()
+    elif option == VIEW_FAVOURITES_ARG:
+        print_favourites(favourite_drinks)
+        wait()
+    else:
+        output(f'"{option}"" is not an option that I recognise')
         wait()
