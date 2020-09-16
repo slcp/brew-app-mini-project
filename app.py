0473c4e356a9da1c99a38b712c5aa08afd08f1b0
Stuart Forrest <stuart.forrest@infinityworks.com>
2020-09-16 08:30:55 +0100

    add funcs to be able to print a table with content to the terminal

--- app.py
+++ app.py
@@ -1,7 +1,40 @@
-from sys import argv
-args = argv
+from sys import argv as args
 
-# Define data
+
+def get_table_width(items):
+    longest = 0
+    for item in items:
+        if len(item) > longest:
+            longest = len(item)
+    return longest + 2
+
+
+def print_table(title, contents):
+    width = get_table_width(contents)
+    print_table_header(title, width)
+    print_table_body(contents)
+    print_table_footer(width)
+
+
+def print_table_body(contents):
+    for item in contents:
+        print(f'| {item}')
+
+
+def print_divider(length):
+    print(f'+{"=" * length}+')
+
+
+def print_table_header(title, width):
+    print_divider(width)
+    print(f'| {title.upper()}')
+    print_divider(width)
+
+
+def print_table_footer(width):
+    print_divider(width)
+
+
 GET_PEOPLE_ARG = 'get-people'
 GET_DRINKS_ARG = 'get-drinks'
 
@@ -21,9 +54,8 @@ if args[1] not in [GET_DRINKS_ARG, GET_PEOPLE_ARG]:
     exit()
 
 # Handle command
-if args[1] == GET_DRINKS_ARG:
-    for drink in drinks:
-        print(drink)
-elif args[1] == GET_PEOPLE_ARG:
-    for person in people:
-        print(person)
+command = args[1]
+if command == GET_DRINKS_ARG:
+    print_table('drinks', drinks)
+elif command == GET_PEOPLE_ARG:
+    print_table('people', people)
