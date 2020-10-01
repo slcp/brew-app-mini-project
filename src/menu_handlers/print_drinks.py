0daa27c3936dc4b472263995990428811adff432
Stuart Forrest <stuart.forrest@infinityworks.com>
2020-10-01 08:26:18 +0100

    use correct db

--- src/menu_handlers/print_drinks.py
+++ src/menu_handlers/print_drinks.py
@@ -3,6 +3,6 @@ from src.core.output import print_drinks_table
 
 def make_handle_get_drinks(file_db, sql_db):
     def handler():
-        drinks = sql_db.load_drinks()
+        drinks = file_db.load_drinks()
         print_drinks_table(drinks)
     return handler
