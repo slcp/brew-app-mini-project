3580c41668958780d1a61c14d816110a3a442a90
Stuart Forrest <stuart.forrest@infinityworks.com>
2020-10-02 11:24:15 +0000

    added func to insert a drink into db

--- src/data_store/mysql_db.py
+++ src/data_store/mysql_db.py
@@ -70,7 +70,23 @@ class MySQLDB:
                         person.last_name, person.age]
                 sql = f'INSERT INTO {PERSON_TABLE} ({PERSON_ID_COLUMN}, {PERSON_FIRST_NAME_COLUMN}, \
                     {PERSON_LAST_NAME_COLUMN}, {PERSON_AGE_COLUMN}) VALUES (%s, %s, %s, %s)'
-                cursor.execute(sql, [*data])
+                cursor.execute(sql, data)
                 connection.commit()
+        finally:
+            connection.close()
+
+    # Save drinks - match input/output to save to file drinks function
+
+    def insert_drink(self, drink):
+        connection = self.__make_connection()
+        try:
+            with connection.cursor() as cursor:
+                data = [drink.id, drink.name]
+                sql = 'INSERT INTO drink (id, name) VALUES (%s, %s)'
+                cursor.execute(sql, data)
+                connection.commit()
+        except:
+             # Handle error
+             pass
         finally:
             connection.close()
